5afe899fb68af990b807c1aaba08ee2c310af3ab
Patrick Shim <shimp@oregonstate.edu>
2021-01-07 04:51:52 -0500

    added defer to script

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 // Gets input from input field
 function getUserNumberInput(){
@@ -10,12 +11,15 @@ function getUserNumberInput(){
 function createAndWriteOutput(resultBeforeCalc, operator, resultAfterNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${resultAfterNumber}`; 
     outputResult(currentResult, calcDescription); //from vendor.js
+}
 
 function add() {
     const initialResult = currentResult;
     const enteredNumber = getUserNumberInput();
     currentResult += enteredNumber;
     createAndWriteOutput(initialResult, '+', enteredNumber);
+    logEntries.push(enteredNumber);
+    console.log(logEntries[0]);
 }
 
 function subtract() {
