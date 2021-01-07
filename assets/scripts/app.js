25ed75a78f0cf71a405d221f351c53e52aa3d358
Patrick Shim <shimp@oregonstate.edu>
2021-01-07 03:07:04 -0500

    added add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,25 @@
-let currentResult = ""
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput(){
+    return parseInt(userInput.value);
+}
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`; 
+    currentResult = currentResult + parseInt(enteredNumber);
+    outputResult(currentResult, calcDescription);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`; 
+    currentResult = currentResult + parseInt(enteredNumber);
+    outputResult(currentResult, calcDescription);
+}
+
+
+
+
+addBtn.addEventListener('click', add);
