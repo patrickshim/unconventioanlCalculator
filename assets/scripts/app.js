4c3bffa2d741b2c02f3729367d0b45733123df0c
Patrick Shim <shimp@oregonstate.edu>
2021-01-07 03:34:28 -0500

    added other buttons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,21 +5,40 @@ function getUserNumberInput(){
     return parseInt(userInput.value);
 }
 
-function add() {
-    const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`; 
-    currentResult = currentResult + parseInt(enteredNumber);
+function createAndWriteOutput(resultBeforeCalc, operator, resultAfterNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${resultAfterNumber}`; 
     outputResult(currentResult, calcDescription);
+}
+
+function add() {
+    const initialResult = currentResult;
+    const enteredNumber = getUserNumberInput();
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput(initialResult, '+', enteredNumber);
 }
 
 function subtract() {
+    const initialResult = currentResult;
     const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`; 
-    currentResult = currentResult + parseInt(enteredNumber);
-    outputResult(currentResult, calcDescription);
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput(initialResult, '-', enteredNumber);
 }
 
+function multiply() {
+    const initialResult = currentResult;
+    const enteredNumber = getUserNumberInput();
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput(initialResult, '*', enteredNumber)
+}
 
-
+function divide() {
+    const initialResult = currentResult;
+    const enteredNumber = getUserNumberInput();
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput(initialResult, '/', enteredNumber)
+}
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
